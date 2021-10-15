fa544449a65d3994106b07e4e7342f3e5033e86f
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-10-14 21:34:31 -0400

    Complete book assignment

--- src/book.js
+++ src/book.js
@@ -1,22 +1,37 @@
 function createTitle(title) {
   var title = title;
-  console.log (`The ${title}`);
+  return `The ${title}`;
 }
 
-function buildMainCharacter() {
-
+function buildMainCharacter(name, age, pronouns) {
+  return {name:name, age:age, pronouns:pronouns};
 }
 
+function saveReview(review, reviews) {
+ if (!reviews.includes(review)) {
+   reviews.push(review);
+ }
+}
 
+function calculatePageCount(title) {
+  return title.length * 20;
+}
 
+function writeBook(title, char, genre) {
+  return {title: title, mainCharacter: char, pageCount: calculatePageCount(title), genre: genre};
+}
+
+function editBook(book) {
+  book.pageCount *= .75;
+}
 
 
 
 module.exports = {
-  createTitle
+  createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 };
