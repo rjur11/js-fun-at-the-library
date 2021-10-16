f344d4af26fef14e6ad3220f3e35ae44fa1595ac
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-10-16 15:38:29 -0400

    Start librarian

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,33 @@
+function findTitle(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+}
+
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+
+  greetPatron(patronName, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${patronName}!`;
+    }
+    return `Hello, ${patronName}!`;
+  }
+
+  findBook(title) {
+    var genres = ["fantasy", "fiction", "nonFiction"];
+    for (var i = 0; i < genres.length; i++) {
+      if (findTitle(title, this.library.shelves[genres[i]])) {
+        return `Yes, we have ${title}`;
+      }
+    }
+    return `Sorry, we do not have ${title}`;
   }
 }
 
